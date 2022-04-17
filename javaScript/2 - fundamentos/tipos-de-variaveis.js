fd86e7b37ec31b4c188a86cfe2b7b54ff72c517a
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-04-17 15:48:20 -0300

    update - fundamentos, tipos de variaveis

---
+++ javaScript/2 - fundamentos/tipos-de-variaveis.js
@@ -0,0 +1,10 @@
+// No JS temos 3 tipos de variaveis, var, let e const
+
+var number = 0;
+let number2 = 2;
+const number3 = 4;
+
+// o tipo var pode ser inicializada tanto no inicio quanto no fim do projetos, as outras duas não
+// let e var funcionarm igual tirando a inicialização
+// atualmente se usa let, var caiu em desuso pq era mt "livre"
+// const vem de constante, ou seja, ela n pode mudar de valor na aplicação depois do valor atribuido
