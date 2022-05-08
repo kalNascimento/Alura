f559a9006f673577e4ca83e1ba62fd10075c4238
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-05-08 20:02:03 -0300

    Update - atualizado a package p/ aceitar os modulos

--- javaScript/NodeJS-Biblioteca/index.js
+++ javaScript/NodeJS-Biblioteca/index.js
@@ -1,1 +1,10 @@
+import chalk from 'chalk';
+const texto = "texto retornado por uma função";
 
+console.log(chalk.blue('Hello world!'));
+
+function TextoReturn(string){
+    return string;
+}
+
+console.log(TextoReturn(texto));
