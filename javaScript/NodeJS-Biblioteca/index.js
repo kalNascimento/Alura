9e7a2a7be81f03e15c63f95043c7243e3e668ddf
Kalebe Nascimento <kalebe.nascimento19@gmail.com>
2022-05-16 17:18:03 -0300

    Update - Exportada a função principal para rodar no script cli

--- javaScript/NodeJS-Biblioteca/index.js
+++ javaScript/NodeJS-Biblioteca/index.js
@@ -1,10 +1,69 @@
 import chalk from 'chalk';
-const texto = "texto retornado por uma função";
+import fs from 'fs';
 
-console.log(chalk.blue('Hello world!'));
-
-function TextoReturn(string){
-    return string;
+function extrairLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResult = [];
+    const notLinks = chalk.red('não há links')
+    let temp;
+    while((temp = regex.exec(texto)) !== null){
+        arrayResult.push({ [temp[1]]: temp[2]});
+    }
+    return arrayResult.length === 0 ? notLinks : arrayResult
 }
 
-console.log(TextoReturn(texto));
+function errorFile(erro) {
+    throw new Error(chalk.red(erro.code, "Não há arquivo no caminho"))
+}
+
+async function pegaArquivo(pathFile) {
+    const encoding = 'utf-8';
+    try {
+        const texto = await fs.promises.readFile(pathFile, encoding);
+        return extrairLinks(texto);
+    } catch (erro) {
+        errorFile(erro)
+    }
+}
+
+export default pegaArquivo
+
+
+
+
+
+
+
+
+
+
+
+//pegaArquivo('./arquivos/texto.md');
+/* 
+    \[[^\]]*\]\((https?:\/\/[^$#\s].[^\s]*\)
+    
+    */
+
+
+
+
+
+
+/* refatorados p/ async*/
+
+/*function pegaArquivo(pathFile) {
+    const encoding = 'utf-8';
+    fs.promises.readFile(pathFile, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch((erro) => errorFile(chalk.red(erro)))
+}
+
+function pegaArquivo(pathFile) {
+    const encoding = 'utf-8';
+    fs.readFile(pathFile, encoding, (erro, texto) => {
+        if (erro){
+            errorFile(erro);
+        }
+        console.log(chalk.green(texto));
+    })
+}*/
